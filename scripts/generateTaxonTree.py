68b674a74b5a395c0e7293f39a629bb815161a78
camillekrieger <camillekrieger@yahoo.com>
2025-04-04 00:32:21 -0600

    Update generateTaxonTree.py to include full species name

--- scripts/generateTaxonTree.py
+++ scripts/generateTaxonTree.py
@@ -1,15 +1,35 @@
 from Bio import Phylo
 import sys
 import matplotlib.pyplot as plt
+import re
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
+species_dict = {}
+
+def extract_species_names_from_tre(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            matches = re.findall(r'([A-Za-z]+(?:\s[A-Za-z]+)?)', line)
+            for match in matches:
+                name_parts = match.split()
+                if len(name_parts) > 1:
+                    second_word = name_parts[1]
+                    species_dict[second_word] = match
+                else:
+                    species_dict[name_parts[0]] = match
+
+extract_species_names_from_tre(infile)
+
 tree = Phylo.read(infile, "newick")
 
 def format_label(clade):
-    if clade.name:
-        return r"$\mathit{" + clade.name + "}$"
+    if clade.is_terminal():
+        if clade.name:
+            full_species_name = species_dict.get(clade.name, clade.name)
+            full_species_name = full_species_name.replace(" ", "~")
+            return r"$\mathit{" + full_species_name + "}$"
     return None
 
 fig, ax = plt.subplots(figsize=(12, 10))
