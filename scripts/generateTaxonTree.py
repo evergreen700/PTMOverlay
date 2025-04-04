48a5fa99ae0a0eb60ecad90bc60ec6898bd55a66
camillekrieger <camillekrieger@yahoo.com>
2025-04-04 00:11:43 -0600

    Update generateTaxonTree.py to italicize species names

--- scripts/generateTaxonTree.py
+++ scripts/generateTaxonTree.py
@@ -7,8 +7,13 @@ outfile = sys.argv[2]
 
 tree = Phylo.read(infile, "newick")
 
+def format_label(clade):
+    if clade.name:
+        return r"$\mathit{" + clade.name + "}$"
+    return None
+
 fig, ax = plt.subplots(figsize=(12, 10))
-Phylo.draw(tree, do_show=False, axes=ax)
+Phylo.draw(tree, do_show=False, axes=ax, label_func=format_label)
 
 plt.tight_layout()
 plt.savefig(outfile, format="pdf", dpi=300, bbox_inches="tight")
