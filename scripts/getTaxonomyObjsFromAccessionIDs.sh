cd4dcb4e1570020ce1ae6d1eda8acc767c3e23cc
Zachary Everton <33760323+evergreen700@users.noreply.github.com>
2025-04-11 16:57:59 -0600

    Update getTaxonomyObjsFromAccessionIDs.sh
    test

--- scripts/getTaxonomyObjsFromAccessionIDs.sh
+++ scripts/getTaxonomyObjsFromAccessionIDs.sh
@@ -2,24 +2,19 @@
 ACCESSION_ID_PATH=$1
 # the output file
 TAXONOMY_OBJS_PATH=$2
-
 INTERMEDIATE_PATH="/tmp/tree.txt"
-
 if [ -z "$ACCESSION_ID_PATH" ] || [ -z "$INTERMEDIATE_PATH" ]; then
   echo "Incorrect number of arguments. Need at least two paths:"
   echo -e "\tInput Path"
   echo -e "\tOutput Path"
   exit 1
 fi
-
 # clear the contents of the output file
 truncate -s 0 $INTERMEDIATE_PATH
-
 # append a newline character to the end of the file if it doesn't end with one
 if [[ $(tail -c 1 $ACCESSION_ID_PATH) != "" ]]; then
   echo "" >> "$ACCESSION_ID_PATH"
 fi
-
 function processAccessionIDs() {
   echo $numIDs
   # remove the trailing comma and space
@@ -29,7 +24,6 @@ function processAccessionIDs() {
   # print the taxonomyObjs to the output file
   echo -e "$TAXONOMY_OBJS" >> $INTERMEDIATE_PATH
 }
-
 # loop through each accession id and append to a variable
 # process the IDs in large chunks
 numIDs=0
@@ -43,7 +37,5 @@ while read ACCESSION_ID; do
   fi 
 done < "$ACCESSION_ID_PATH"
 processAccessionIDs $ACCESSION_IDS
-
 mv "$TAXONOMY_OBJS_PATH" "$INTERMEDIATE_PATH"
-
 echo "done."
